44cb6c28d0e797a19ae1cb229174602b6e8afb32
samar-almas <samaralmas8@gmail.com>
2023-07-18 19:52:57 +0500

    profile page added

--- src/components/ChatRoom/ChatInput/index.js
+++ src/components/ChatRoom/ChatInput/index.js
@@ -1,9 +1,9 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from "react";
 import { IoMdSend } from "react-icons/io";
 import { useDispatch } from "react-redux";
 
 import socket from "../../../socket";
-import Typing from '../Typing';
+import Typing from "../Typing";
 import { capatalize } from "../../../utils";
 import { sendMessage } from "../../../store/slices/messageSlice";
 
@@ -11,103 +11,120 @@ import styles from "./style.module.scss";
 const { chatInputContainer } = styles;
 
 function ChatInput({ chatId, messageContainerRef, user }) {
-    const dispatch = useDispatch();
-    const [typing, setTyping] = useState(false);
-    const [chatMessage, setMessage] = useState("");
+  const dispatch = useDispatch();
+  const [typing, setTyping] = useState(false);
+  const [chatMessage, setMessage] = useState("");
 
-    const typingTimoutId = useRef(null);
-    const inputRef = useRef(null);
-    const controller = useRef({ abort: () => { } });
+  const typingTimoutId = useRef(null);
+  const inputRef = useRef(null);
+  const controller = useRef({ abort: () => {} });
 
-    function scrollChatToBottom() {
-        if (messageContainerRef.current) {
-            messageContainerRef.current.scrollTop = messageContainerRef.current.scrollHeight;
-        }
+  function scrollChatToBottom() {
+    if (messageContainerRef.current) {
+      messageContainerRef.current.scrollTop =
+        messageContainerRef.current.scrollHeight;
+    }
+  }
+
+  function handleSubmit() {
+    socket.connect();
+
+    const payload = {
+      chatId: chatId,
+      message: chatMessage,
+    };
+
+    if (payload.message) {
+      const promise = dispatch(sendMessage(payload));
+      controller.current.abort = promise.abort;
+
+      promise.unwrap().then((message) => {
+        socket.emit("sendMessage", message);
+
+        scrollChatToBottom();
+        setMessage("");
+      });
+    }
+  }
+
+  function handleChange(e) {
+    setMessage(e.target.value);
+  }
+
+  function startTyping(event) {
+    clearTimeout(typingTimoutId.current);
+
+    socket.connect();
+    console.log(socket);
+
+    socket.emit("typing", { chatId, user });
+
+    if (event.keyCode === 13) {
+      // 13 is the Enter key code
+      event.preventDefault();
+      handleSubmit();
+    }
+  }
+
+  function stopTyping() {
+    socket.connect();
+    console.log(socket);
+
+    clearTimeout(typingTimoutId.current);
+
+    typingTimoutId.current = setTimeout(function () {
+      socket.emit("typingOff", { chatId, user });
+    }, 600);
+  }
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
     }
 
-    function handleSubmit() {
-        socket.connect();
+    socket.on("startTyping", (data) => {
+      if (data.chatId === chatId && data.user._id !== user._id) {
+        setTyping(data.user);
+      }
+    });
 
-        const payload = {
-            chatId: chatId,
-            message: chatMessage
-        };
+    socket.on("stopTyping", (data) => {
+      if (data.chatId === chatId && data.user._id !== user._id) {
+        setTyping(false);
+      }
+    });
 
-        if (payload.message) {
-            const promise = dispatch(sendMessage(payload));
-            controller.current.abort = promise.abort;
+    return () => {
+      controller.current.abort();
+    };
+  }, []);
 
-            promise.unwrap().then((message) => {
-                socket.emit("sendMessage", message);
+  return (
+    <div className={chatInputContainer}>
+      <input
+        type="text"
+        ref={inputRef}
+        value={chatMessage}
+        onKeyUp={stopTyping}
+        onKeyDown={startTyping}
+        onChange={handleChange}
+        placeholder="Type a message"
+      />
 
-                scrollChatToBottom();
-                setMessage("");
-            });
-        }
-    }
+      <button onClick={handleSubmit}>
+        {" "}
+        <IoMdSend />{" "}
+      </button>
 
-    function handleChange(e) {
-        setMessage(e.target.value);
-    }
-
-    function startTyping(event) {
-        clearTimeout(typingTimoutId.current);
-
-        socket.emit("typing", { chatId, user });
-
-        if (event.keyCode === 13) { // 13 is the Enter key code
-            event.preventDefault();
-            handleSubmit();
-        }
-    }
-
-    function stopTyping() {
-        clearTimeout(typingTimoutId.current);
-
-        typingTimoutId.current = setTimeout(function () {
-            socket.emit("typingOff", { chatId, user });
-        }, 600);
-    }
-
-    useEffect(() => {
-        if (inputRef.current) {
-            inputRef.current.focus();
-        }
-
-        socket.on("startTyping", (data) => {
-            if (data.chatId === chatId && data.user._id !== user._id) {
-                setTyping(data.user);
-            }
-        });
-
-        socket.on("stopTyping", (data) => {
-            if (data.chatId === chatId && data.user._id !== user._id) {
-                setTyping(false);
-            }
-        });
-
-        return () => {
-            controller.current.abort();
-        }
-    }, []);
-
-    return (
-        <div className={chatInputContainer}>
-            <input
-                type="text"
-                ref={inputRef}
-                value={chatMessage}
-                onKeyUp={stopTyping}
-                onKeyDown={startTyping}
-                onChange={handleChange}
-                placeholder='Type a message'
-            />
-
-            <button onClick={handleSubmit}> <IoMdSend /> </button>
-
-            {typing && (<Typing name={`${capatalize(typing.firstName)} ${capatalize(typing.lastName)}`} />)}
-        </div>
-    );
+      {typing && (
+        <Typing
+          name={`${capatalize(typing.firstName)} ${capatalize(
+            typing.lastName
+          )}`}
+        />
+      )}
+    </div>
+  );
 }
 
 export default React.memo(ChatInput);
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,194 @@
-import React from 'react';
+import React from "react";
+import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
+import { useEffect, useRef, useState } from "react";
+import Alert from "react-bootstrap/Alert";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
 
-function Profile() {  
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+// import RequestLoader from "./../RequestLoader";
+// import { login, authActions } from "../../store/slices/authSlice";
+
+const ProfilePage = () => {
   return (
-    <>
-        <h1>Profile Page</h1>
-    </>
-  )
-}
+    <Container className="px-4 mt-4">
+      <Row>
+        <Col xl={4}>
+          {/* Profile picture card */}
+          <Card className="mb-4 mb-xl-0">
+            <Card.Header>Profile Picture</Card.Header>
+            <Card.Body className="text-center">
+              {/* Profile picture image */}
+              <img
+                className="img-account-profile rounded-circle mb-2"
+                src="http://bootdey.com/img/Content/avatar/avatar1.png"
+                alt=""
+              />
+              {/* Profile picture help block */}
+              <div className="small font-italic text-muted mb-4">
+                JPG or PNG no larger than 5 MB
+              </div>
+              {/* Profile picture upload button */}
+              <Button variant="primary" type="button">
+                Upload new image
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xl={8}>
+          {/* Account details card */}
+          <Card className="mb-4">
+            <Card.Header>Account Details</Card.Header>
+            <Card.Body>
+              <Form>
+                {/* Form Group (username) */}
+                <Form.Group className="mb-3">
+                  <Form.Label className="small mb-1" htmlFor="inputUsername">
+                    Username (how your name will appear to other users on the
+                    site)
+                  </Form.Label>
+                  <Form.Control
+                    id="inputUsername"
+                    type="text"
+                    placeholder="Enter your username"
+                    defaultValue="username"
+                  />
+                </Form.Group>
+                {/* Form Row */}
+                <Row className="gx-3 mb-3">
+                  {/* Form Group (first name) */}
+                  <Col md={6}>
+                    <Form.Group className="mb-3">
+                      <Form.Label
+                        className="small mb-1"
+                        htmlFor="inputFirstName"
+                      >
+                        First name
+                      </Form.Label>
+                      <Form.Control
+                        id="inputFirstName"
+                        type="text"
+                        placeholder="Enter your first name"
+                        defaultValue="Valerie"
+                      />
+                    </Form.Group>
+                  </Col>
+                  {/* Form Group (last name) */}
+                  <Col md={6}>
+                    <Form.Group className="mb-3">
+                      <Form.Label
+                        className="small mb-1"
+                        htmlFor="inputLastName"
+                      >
+                        Last name
+                      </Form.Label>
+                      <Form.Control
+                        id="inputLastName"
+                        type="text"
+                        placeholder="Enter your last name"
+                        defaultValue="Luna"
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                {/* Form Row */}
+                <Row className="gx-3 mb-3">
+                  {/* Form Group (organization name) */}
+                  <Col md={6}>
+                    <Form.Group className="mb-3">
+                      <Form.Label className="small mb-1" htmlFor="inputOrgName">
+                        Organization name
+                      </Form.Label>
+                      <Form.Control
+                        id="inputOrgName"
+                        type="text"
+                        placeholder="Enter your organization name"
+                        defaultValue="Start Bootstrap"
+                      />
+                    </Form.Group>
+                  </Col>
+                  {/* Form Group (location) */}
+                  <Col md={6}>
+                    <Form.Group className="mb-3">
+                      <Form.Label
+                        className="small mb-1"
+                        htmlFor="inputLocation"
+                      >
+                        Location
+                      </Form.Label>
+                      <Form.Control
+                        id="inputLocation"
+                        type="text"
+                        placeholder="Enter your location"
+                        defaultValue="San Francisco, CA"
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                {/* Form Group (email address) */}
+                <Form.Group className="mb-3">
+                  <Form.Label
+                    className="small mb-1"
+                    htmlFor="inputEmailAddress"
+                  >
+                    Email address
+                  </Form.Label>
+                  <Form.Control
+                    id="inputEmailAddress"
+                    type="email"
+                    placeholder="Enter your email address"
+                    defaultValue="name@example.com"
+                  />
+                </Form.Group>
+                {/* Form Row */}
+                <Row className="gx-3 mb-3">
+                  {/* Form Group (phone number) */}
+                  <Col md={6}>
+                    <Form.Group className="mb-3">
+                      <Form.Label className="small mb-1" htmlFor="inputPhone">
+                        Phone number
+                      </Form.Label>
+                      <Form.Control
+                        id="inputPhone"
+                        type="tel"
+                        placeholder="Enter your phone number"
+                        defaultValue="555-123-4567"
+                      />
+                    </Form.Group>
+                  </Col>
+                  {/* Form Group (birthday) */}
+                  <Col md={6}>
+                    <Form.Group className="mb-3">
+                      <Form.Label
+                        className="small mb-1"
+                        htmlFor="inputBirthday"
+                      >
+                        Birthday
+                      </Form.Label>
+                      <Form.Control
+                        id="inputBirthday"
+                        type="text"
+                        name="birthday"
+                        placeholder="Enter your birthday"
+                        defaultValue="06/10/1988"
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+                {/* Save changes button */}
+                <Button variant="primary" type="button">
+                  Save changes
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default Profile;
+export default ProfilePage;
