f04ebb124f9bf2564f4b669bbc7956e420adff33
Muhammad Abubakar <90tricks90@gmail.com>
2023-07-25 09:55:17 +0500

    Addding profile file validation

--- src/components/ProfilePictureCard/index.js
+++ src/components/ProfilePictureCard/index.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Card, Button } from "react-bootstrap";
 
 import styles from "./style.module.scss";
-const { profilePic } = styles;
+const { profilePic, fileInput } = styles;
 
-function ProfilePictureCard() {
+function ProfilePictureCard({ avatar }) {
+    const fileInputRef = useRef(null);
+    const [selectedImage, setSelectedImage] = useState(avatar);
+
+    function handleUpload(event) {
+        if (event.target.files.length === 0) {
+            alert("Please select an image to upload.");
+            event.preventDefault();
+            return;
+        }
+
+        const file = event.target.files[0];
+        const allowedTypes = ["image/jpeg", "image/png"];
+        if (!allowedTypes.includes(file.type)) {
+            alert("Please upload a JPG or PNG image.");
+            event.preventDefault();
+            return;
+        }
+
+        const maxSize = 5 * 1024 * 1024; // 5 MB in bytes
+        if (file.size > maxSize) {
+            alert("Please upload an image smaller than 5 MB.");
+            event.preventDefault();
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.onloadend = () => setSelectedImage(reader.result);
+        reader.readAsDataURL(file);
+    }
+
     return (
         <Card className="mb-4 mb-xl-0">
             <Card.Header>Profile Picture</Card.Header>
 
             <Card.Body className="text-center">
-                <div className={profilePic}>
+                <div className={profilePic} onClick={() => fileInputRef.current.click()}>
                     <img
-                        src="http://bootdey.com/img/Content/avatar/avatar1.png"
                         className="rounded-circle"
                         alt="profile-picture"
+                        src={selectedImage}
                     />
                 </div>
 
+                <input
+                    type="file"
+                    ref={fileInputRef}
+                    className={fileInput}
+                    onChange={handleUpload}
+                />
+
                 <div className="small font-italic text-muted mb-3"> JPG or PNG no larger than 5 MB </div>
-    
-                <Button variant="primary" type="button"> Upload new image </Button>
+
+                <Button variant="primary" type="button"> Update Profile Avatar </Button>
             </Card.Body>
         </Card>
     );
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 
 import Header from "../components/Header";
@@ -6,16 +7,18 @@ import ProfileForm from "../components/ProfileForm";
 import ProfilePictureCard from "../components/ProfilePictureCard";
 
 const ProfilePage = () => {
+  const user = useSelector((state) => state.user.user);
+
   return (
     <>
       <Header />
 
       <Row className="p-4 mx-0">
         <Col xl={4} className="ps-0">
-          <ProfilePictureCard />
+          <ProfilePictureCard avatar={user.avatar} />
         </Col>
         <Col xl={8} className="pe-0">
-          <ProfileForm />
+          <ProfileForm user={user} />
         </Col>
       </Row>
     </>
